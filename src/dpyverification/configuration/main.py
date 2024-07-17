b5f07db2177ab271ddebcce0b837f624653323c8
Aljen Uitbeijerse <aljen.uitbeijerse@deltares.nl>
2024-07-17 13:48:33 +0200

    Add datasources specification to configuration

--- src/dpyverification/configuration/main.py
+++ src/dpyverification/configuration/main.py
@@ -4,14 +4,9 @@ import pathlib
 from enum import Enum
 
 import yaml
-from jsonschema import validate
+from jsonschema import ValidationError, validate
 
-_schema = {
-    "type": "object",
-    "properties": {
-        "fileversion": {"type": "string"},
-    },
-}
+from .schema import YAMLSCHEMA
 
 
 class ConfigTypes(Enum):
@@ -31,16 +26,26 @@ class Config:
     def __init__(self, configfile: pathlib.Path, configtype: ConfigTypes) -> None:
         if configtype is ConfigTypes.RUNINFO:
             # parse the runinfo into a yaml
-            yamlcontent = {"fileversion": "0.0.1"}
+            yamlcontent = {
+                "fileversion": "0.0.1",
+                "datasources": [{"datasourcetype": "fewswebservice", "url": "fake"}],
+            }
         elif configtype is ConfigTypes.YAML:
             with configfile.open() as cf:
                 yamlcontent = yaml.safe_load(cf)
-
-        # conversion from older fileversion to current schema
-        # NOT IMPLEMENTED YET, because we have not had a fileversion update
+            # conversion from older fileversion to current schema
+            # NOT IMPLEMENTED YET, because we have not had a fileversion update
 
         # check the yaml
-        validate(yamlcontent, _schema)
+        try:
+            validate(yamlcontent, YAMLSCHEMA)
+        except ValidationError as error:  # type: ignore[misc]
+            if error.parent:
+                # When there is an error in an anyOf schema part, the parent often has a clearer
+                # description of the problem
+                raise error.parent from error
+            raise
 
         self.filename = configfile
         self.configtype = configtype
+        self.datasources = yamlcontent["datasources"]
---
+++ src/dpyverification/configuration/schema.py
@@ -0,0 +1,40 @@
+"""The schema definition for the configuration yaml file."""
+
+_FEWSWEBSERVICE = {
+    "type": "object",
+    "required": ["datasourcetype", "url"],
+    "properties": {
+        "datasourcetype": {"type": "string", "const": "fewswebservice"},
+        "url": {"type": "string"},
+    },
+}
+
+_LOCALFILE = {
+    "type": "object",
+    "required": ["datasourcetype", "filetype", "directory", "filename"],
+    "properties": {
+        "datasourcetype": {"type": "string", "const": "localfile"},
+        "filetype": {"enum": ["pixml", "fewsnetcdf"]},
+        "directory": {"type": "string"},
+        "filename": {"type": "string"},
+    },
+}
+
+YAMLSCHEMA = {
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "object",
+    "properties": {
+        "fileversion": {"type": "string"},
+        "datasources": {
+            "type": "array",
+            "minItems": 1,
+            "items": {
+                "anyOf": [
+                    _FEWSWEBSERVICE,
+                    _LOCALFILE,
+                ],
+            },
+        },
+    },
+    "required": ["fileversion", "datasources"],
+}
