98ca3df4ab25f6e2f9998af5601c29641093421a
jurianbeunk <jurianbeunk@gmail.com>
2026-02-24 08:18:26 +0100

    Update schema export to check if file already exists

--- scripts/export_versioned_schemas.py
+++ scripts/export_versioned_schemas.py
@@ -6,12 +6,18 @@ from dpyverification.configuration import Config
 from dpyverification.constants import VERSION
 
 schema_dir = Path(__file__).parent.parent / "schemas" / f"{VERSION}"
-schema_dir.mkdir()
-
 schema_path = schema_dir / "config.schema.json"
 
+# Check if a folder for this version already exists
 if schema_path.exists():
-    msg = f"Schema already exists. Please ensure no schema exists at: {schema_path}"
+    msg = (
+        f"For software version {VERSION} a schema already exists. "
+        f"Please ensure no schema exists at: {schema_path}"
+    )
     raise ValueError(msg)
-else:
-    Config.write_schema(schema_path)
+
+# Make directory and define path
+schema_dir.mkdir(exist_ok=True)
+
+# Write
+Config.write_schema(schema_path)
